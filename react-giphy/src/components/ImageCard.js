6af69adeec1d482893066fd1064d64dbda7dba3c
César Guerra <warderer@gmail.com>
2022-03-16 21:24:17 -0600

    🐛 Corregimos src de la imagen

--- react-giphy/src/components/ImageCard.js
+++ react-giphy/src/components/ImageCard.js
@@ -3,9 +3,10 @@ import React from 'react';
 class ImageCard extends React.Component {
 
     render(){
+        console.log()
         return(
             <div className="image-container">
-                <img src="{this.props.url}" alt="" className="image-view" />
+                <img src={this.props.url} alt="" className="image-view" />
             </div>
         )
     }
