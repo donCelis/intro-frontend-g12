bf550828476011835a68c1776eed18040e83f807
Alexander Celis <alexcelis019@gmail.com>
2022-03-22 22:32:37 -0500

    add: agregar variables de entorno

--- vite-react-app/src/components/App/index.jsx
+++ vite-react-app/src/components/App/index.jsx
@@ -1,6 +1,9 @@
 import './app.scss'
 
 export const App = () => {
+  const apiKey = import.meta.env.APP_SOME_KEY
+
+  console.log(apiKey)
   return (
     <p className='text'>Componente funcional</p>
   )
