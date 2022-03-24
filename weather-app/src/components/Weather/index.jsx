0b4c7f9eb7ea0636412687bcc1339d7209afefb9
Alexander Celis <alexcelis019@gmail.com>
2022-03-23 22:59:20 -0500

    mod: refactor to css

--- weather-app/src/components/Weather/index.jsx
+++ weather-app/src/components/Weather/index.jsx
@@ -8,11 +8,13 @@ export const Weather = () => {
       <div className='weather-side'>
         <div className='weather-gradient' />
         <div className='date-container'>
-          <h2 className='date-dayname'>Tuesday</h2><span className='date-day'>15 Jan 2019</span>
+          <h2 className='date-dayname'>Tuesday</h2>
+          <span className='date-day'>15 Jan 2019</span>
           <FeatherIcon className='location-icon' icon='map-pin' />
           <span className='location'>Paris, FR</span>
         </div>
-        <div className='weather-container'><i className='weather-icon' data-feather='sun' />
+        <div className='weather-container'>
+          <FeatherIcon className='weather-icon' icon='sun' />
           <h1 className='weather-temp'>29°C</h1>
           <h3 className='weather-desc'>Sunny</h3>
         </div>
@@ -20,24 +22,42 @@ export const Weather = () => {
       <div className='info-side'>
         <div className='today-info-container'>
           <div className='today-info'>
-            <div className='precipitation'> <span className='title'>PRECIPITATION</span><span className='value'>0 %</span>
-              <div className='clear' />
+            <div className='precipitation'>
+              <span className='title'>PRECIPITATION</span>
+              <span className='value'>0 %</span>
             </div>
-            <div className='humidity'> <span className='title'>HUMIDITY</span><span className='value'>34 %</span>
-              <div className='clear' />
+            <div className='humidity'>
+              <span className='title'>HUMIDITY</span>
+              <span className='value'>34 %</span>
             </div>
-            <div className='wind'> <span className='title'>WIND</span><span className='value'>0 km/h</span>
-              <div className='clear' />
+            <div className='wind'>
+              <span className='title'>WIND</span>
+              <span className='value'>0 km/h</span>
             </div>
           </div>
         </div>
         <div className='week-container'>
           <ul className='week-list'>
-            <li className='active'><i className='day-icon' data-feather='sun' /><span className='day-name'>Tue</span><span className='day-temp'>29°C</span></li>
-            <li><i className='day-icon' data-feather='cloud' /><span className='day-name'>Wed</span><span className='day-temp'>21°C</span></li>
-            <li><i className='day-icon' data-feather='cloud-snow' /><span className='day-name'>Thu</span><span className='day-temp'>08°C</span></li>
-            <li><i className='day-icon' data-feather='cloud-rain' /><span className='day-name'>Fry</span><span className='day-temp'>19°C</span></li>
-            <div className='clear' />
+            <li className='active'>
+              <FeatherIcon className='day-icon' icon='sun' />
+              <span className='day-name'>Tue</span>
+              <span className='day-temp'>29°C</span>
+            </li>
+            <li>
+              <FeatherIcon className='day-icon' icon='cloud' />
+              <span className='day-name'>Wed</span>
+              <span className='day-temp'>21°C</span>
+            </li>
+            <li>
+              <FeatherIcon className='day-icon' icon='cloud-snow' />
+              <span className='day-name'>Thu</span>
+              <span className='day-temp'>08°C</span>
+            </li>
+            <li>
+              <FeatherIcon className='day-icon' icon='cloud-rain' />
+              <span className='day-name'>Fry</span>
+              <span className='day-temp'>19°C</span>
+            </li>
           </ul>
         </div>
         <div className='location-container'>
