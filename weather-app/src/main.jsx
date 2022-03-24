60329ee15ca3a9a1791f6a9530e72a307440f937
Alexander Celis <alexcelis019@gmail.com>
2022-03-23 22:26:39 -0500

    add: intregrar la vista en react

--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
+import { Weather } from './components/Weather'
 import './styles/index.css'
 
 render(
   <StrictMode>
-    <p>App</p>
+    <Weather />
   </StrictMode>,
   document.getElementById('root')
 )
