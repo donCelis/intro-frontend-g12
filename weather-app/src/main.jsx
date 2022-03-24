4abe81ed776ec5d585cb0fed62c6395330e2cda6
Alexander Celis <alexcelis019@gmail.com>
2022-03-23 21:53:43 -0500

    mod: reset project and install eslint

---
+++ weather-app/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import './styles/index.css'
+
+render(
+  <StrictMode>
+    <p>App</p>
+  </StrictMode>,
+  document.getElementById('root')
+)
