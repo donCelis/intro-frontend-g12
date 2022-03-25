2d2384f4f3abd8b87fea6e8f5a226c3285ff717f
Alexander Celis <alexcelis019@gmail.com>
2022-03-24 22:21:13 -0500

    add: pintar la información de la api en el componente

--- weather-app/src/components/Weather/index.jsx
+++ weather-app/src/components/Weather/index.jsx
@@ -8,6 +8,18 @@ export const Weather = () => {
   const [city, setCity] = useState({})
   const [loading, setLoading] = useState(true)
 
+  const optionsDay = {
+    weekday: 'long'
+  }
+
+  const optionsDate = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+
+  const convertDay = (date, options) => new Date(date).toLocaleDateString('es', options)
+
   useEffect(() => {
     const data = async () => {
       const res = await fetchCity('medellin')
@@ -24,15 +36,19 @@ export const Weather = () => {
         <div className='weather-side'>
           <div className='weather-gradient' />
           <div className='date-container'>
-            <h2 className='date-dayname'>Tuesday</h2>
-            <span className='date-day'>15 Jan 2019</span>
+            <h2 className='date-dayname'>{convertDay(city.list[0].dt_txt, optionsDay)}</h2>
+            <span className='date-day'>{convertDay(city.list[0].dt_txt, optionsDate)}</span>
             <FeatherIcon className='location-icon' icon='map-pin' />
-            <span className='location'>{city.city.name}, FR</span>
+            <span className='location'>{city.city.name}, {city.city.country}</span>
           </div>
           <div className='weather-container'>
-            <FeatherIcon className='weather-icon' icon='sun' />
-            <h1 className='weather-temp'>29°C</h1>
-            <h3 className='weather-desc'>Sunny</h3>
+            <img
+              className='weather-icon'
+              src={`http://openweathermap.org/img/wn/${city.list[0].weather[0].icon}@2x.png`}
+              alt=''
+            />
+            <h1 className='weather-temp'>{city.list[0].main.temp.toFixed(0)}°C</h1>
+            <h3 className='weather-desc'>{city.list[0].weather[0].description}</h3>
           </div>
         </div>
         <div className='info-side'>
@@ -40,40 +56,27 @@ export const Weather = () => {
             <div className='today-info'>
               <div className='precipitation'>
                 <span className='title'>PRECIPITATION</span>
-                <span className='value'>0 %</span>
+                <span className='value'>{city.list[0].pop * 100} %</span>
               </div>
               <div className='humidity'>
                 <span className='title'>HUMIDITY</span>
-                <span className='value'>34 %</span>
+                <span className='value'>{city.list[0].main.humidity} %</span>
               </div>
               <div className='wind'>
                 <span className='title'>WIND</span>
-                <span className='value'>0 km/h</span>
+                <span className='value'>{city.list[0].wind.speed} mt/s</span>
               </div>
             </div>
           </div>
           <div className='week-container'>
             <ul className='week-list'>
-              <li className='active'>
-                <FeatherIcon className='day-icon' icon='sun' />
-                <span className='day-name'>Tue</span>
-                <span className='day-temp'>29°C</span>
-              </li>
-              <li>
-                <FeatherIcon className='day-icon' icon='cloud' />
-                <span className='day-name'>Wed</span>
-                <span className='day-temp'>21°C</span>
-              </li>
-              <li>
-                <FeatherIcon className='day-icon' icon='cloud-snow' />
-                <span className='day-name'>Thu</span>
-                <span className='day-temp'>08°C</span>
-              </li>
-              <li>
-                <FeatherIcon className='day-icon' icon='cloud-rain' />
-                <span className='day-name'>Fry</span>
-                <span className='day-temp'>19°C</span>
-              </li>
+              {city.list.map((day, index) =>
+                <li key={index} className='active'>
+                  <FeatherIcon className='day-icon' icon='sun' />
+                  <span className='day-name'>Tue</span>
+                  <span className='day-temp'>{day.main.temp.toFixed(0)}°C</span>
+                </li>
+              )}
             </ul>
           </div>
           <div className='location-container'>
